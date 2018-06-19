57abba4f0394d822f3b0b358a690723e3faea684
Priyanka Danda <priyankadanda47@gmail.com>
2018-06-19 23:40:36 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,19 +1,19 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray (a,b)
 {
-  
+  return [b,...a]
 }
 function destructivelyAddElementToBeginningOfArray (a,b)
 {
-  
+  return a.unshift(b)
 }
 function addElementToEndOfArray (a,b)
 {
-  
+  return [...a,b]
 }
 function destructivelyAddElementToEndOfArray (a,b)
 {
-
+return a.push(b)
 }
 function accessElementInArray (a,i)
 {
